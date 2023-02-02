633915349ec2c2aef50e3847fb71b1b18ada3cde
humam-hossain <humam.hossain.e@gmail.com>
2023-02-02 14:03:01 +0600

    react-native-ui-kitten added

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>DS App</Text>
+      <Button />
     </View>
   );
 }
